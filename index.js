fca4bc3b2f35969c981b13fe6a3a3e5ca05a6f44
puppyyoho <bananapuppy.2025@gmail.com>
2026-09-03 08:12:13 +0800

    Improve legacy 1.14 modal layout

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ import {
 const EXTENSION_NAME = 'persona-forge';
 const DISPLAY_NAME = '嘎嘎人设生成器';
 const SETTINGS_KEY = 'personaForge';
-const VERSION = '0.7.2';
+const VERSION = '0.7.3';
 const FAB_ICON_URL = new URL('./icon.png', import.meta.url).href;
 const DEFAULT_FAB_SIZE = 65;
 const MAX_LORE_CHARS_DEFAULT = 52000;
